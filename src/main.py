5ad51f737f83a664262b7f259f3034de78f02c25
ahmedzaky2 <ahmedzakymouad303@gmail.com>
2025-06-20 15:57:26 +0300

    Mongo Schemes and Models

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ async def startup_db_client():
 @app.on_event("shutdown")
 async def shutdown_db_client():
     app.mongo_conn.close()
-    
-    
+
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
